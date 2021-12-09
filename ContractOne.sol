a4534f2568e46fc13c21900aa7f66a551fd20105
Israel <israeladefidipe@gmail.com>
2021-12-09 23:23:56 +0100

    added more explanation to contract

--- ContractOne.sol
+++ ContractOne.sol
@@ -1,9 +1,11 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.17; //specifies the version of solidity you are writing in, the compiler compiles the code in this specified version
 
-contract Inbox {
-    string public message;
+contract Inbox { // contract declaration: see this as class in JS, when it is delpoyed on the ETH network the contract account on the network is an instance of this contractg source
 
-    function Inbox(string initialMessage) public {
+    string public message; // this a STORAGE VARIABLE/INSTANCE variable: its going to exist for the life of the contract
+
+
+    function Inbox(string initialMessage) public { //constructor function: runs once ths contract is called
         message = initialMessage;
     }
 
