a1fe949676497ddc3e38e4c1e34e462f8b464ed2
Israel <israeladefidipe@gmail.com>
2022-01-15 19:49:37 +0100

    completed inbox contract v-0.4.7

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,11 +1,12 @@
 pragma solidity ^0.4.17;
 
+// simple contract 
 contract Inbox {
-    string public message;
+    string public message; //state variable
     
     function Inbox(string initialMessage) public {
         message = initialMessage;
-    }
+    } //constructor
     
     function setMessage(string newMessage) public {
         message = newMessage;
